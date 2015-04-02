fcc7ca1904b1fb8146d1ba1f1a1346bf4958bd18
think <microhuang@live.com>
2015-04-02 10:26:57 +0800

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -2,3 +2,6 @@
 
 #在全部gz文件中查找字符串，并输出所在文件名
 for i in `ls /tmp/*.gz`;do zgrep "zh/supplier-29997_product-21719265.html" $i&&echo $i;done
+
+#
+find /tmp/ -name "*.xml.gz" -exec zgrep "en/cas-registry-number_8-1065.html" {} \;
