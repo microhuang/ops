578ce7c89629f5bb3dda8109dc9d9c12ba54d93a
think <microhuang@live.com>
2015-10-29 18:58:15 +0800

    支持u、v格式化
    取代内置date函数

---
+++ 时间格式化.php
@@ -0,0 +1,14 @@
+<?php
+function vudate($format, $utimestamp = null)
+{
+    if (is_null($utimestamp))
+        $utimestamp = microtime(true);
+    $timestamp = floor($utimestamp);
+    $milliseconds = round(($utimestamp - $timestamp) * 1000);
+    $microseconds = round(($utimestamp - $timestamp) * 1000000);
+    $date = date(preg_replace(array('`(?<!\\\\)v`','`(?<!\\\\)u`'), array($milliseconds,$microseconds), $format), $timestamp);
+    return $date;
+}
+
+echo vudate('Y-m-d H:i:s.u',microtime(true));
+echo "\n";exit;
