5077018d486672603dba98d8494de390f488719d
think <microhuang@live.com>
2016-08-03 17:00:01 +0800

    Create js乘法.mk

---
+++ js乘法.mk
@@ -0,0 +1,10 @@
+```
+  function accMul(arg1,arg2)   
+  {   
+    var m=0,s1=arg1.toString(),s2=arg2.toString();   
+    try{m+=s1.split(".")[1].length}catch(e){}   
+    try{m+=s2.split(".")[1].length}catch(e){}   
+    return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)   
+  }
+  //accMul(2.01,100)
+```
